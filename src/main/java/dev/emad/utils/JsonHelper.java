98583824073575ab32b3361b1f67e30fa8f24a78
Emad Hanif <contact@emadhanif.com>
2024-04-03 06:17:31 +0500

    Modifying JsonHelper

--- src/main/java/dev/emad/utils/JsonHelper.java
+++ src/main/java/dev/emad/utils/JsonHelper.java
@@ -1,5 +1,6 @@
 package dev.emad.utils;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -26,23 +27,29 @@ public class JsonHelper {
     objectMapper.registerModule(new Jdk8Module());
     objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss"));
     objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+    // Excluding properties with null values when serializing...
+    objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
   }
 
   public static String convertToString(Object object) {
     try {
-      ObjectWriter ow = objectMapper.writer().withDefaultPrettyPrinter();
-      return ow.writeValueAsString(object);
+      return objectMapper.writeValueAsString(object);
     } catch (JsonProcessingException e) {
-      throw new RuntimeException("Failed to convert object to Json: " + e.getMessage());
+      throw new RuntimeException("Failed to convert object to JSON: " + e.getMessage());
+    }
+  }
+
+  public static String convertToStringPretty(Object object) {
+    try {
+      return objectMapper.writer().withDefaultPrettyPrinter().writeValueAsString(object);
+    } catch (JsonProcessingException e) {
+      throw new RuntimeException("Failed to convert object to JSON: " + e.getMessage());
     }
   }
 
   public static <T> T convertToObject(Map<String, Object> body, Class<T> clazz) {
-    try {
-      return objectMapper.readValue(objectMapper.writeValueAsString(body), clazz);
-    } catch (JsonProcessingException e) {
-      throw new RuntimeException("Failed to convert Map to object: " + e.getMessage());
-    }
+      return objectMapper.convertValue(body, clazz);
   }
 
   public static <T> T convertToObject(String jsonString, Class<T> clazz) {
@@ -53,13 +60,12 @@ public class JsonHelper {
     }
   }
 
-  public static String prettyPrint(String jsonString) {
+  public static boolean isValidJson(String jsonString) {
     try {
-      JsonNode jsonNode = objectMapper.readTree(jsonString);
-      return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonNode);
+      objectMapper.readTree(jsonString);
+      return true;
     } catch (JsonProcessingException e) {
-      throw new RuntimeException(
-          "Failed to convert object to pretty-printed Json: " + e.getMessage());
+      return false;
     }
   }
 }
